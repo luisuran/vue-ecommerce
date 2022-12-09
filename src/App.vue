abb7395e1d938a128e3e1199fa9d9f130a33f32a
Luis Urán <luisuran.bit@gmail.com>
2022-12-09 11:35:37 -0300

    componente login

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <v-app></v-app>
+  <v-app>
+    <v-snackbar v-model="snackbar" timeout=1500 :color="snack_color">
+      {{ snack_message }}
+    </v-snackbar>
+    <login-form :users="users" @login="logged_user = $event" @show_message="snackbar = $event" @login_message="snack_message = $event" @login_color="snack_color = $event"/>
+  </v-app>
 </template>
 
 <script>
+import LoginForm from './components/LoginForm.vue';
 
 export default {
   name: 'App',
 
   components: {
+    LoginForm,
   },
 
   data: () => ({
+    users: [
+      {
+        id: 1,
+        name: 'Luis Uran',
+        email: 'luisuran.bit@gmail.com',
+        password: '123456',
+      },
+      {
+        id: 2,
+        name: 'Pepe',
+        email: 'pepe@gmail.com',
+        password: '123',
+      }
+    ],
+    logged_user : null,
+    snackbar: false,
+    snack_message: "",
+    snack_color: "",
   }),
 };
 </script>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,48 @@
+<template>
+        <v-card class="mx-auto my-12">
+            <v-card-title>Ingreso</v-card-title>
+            <v-card-text>
+                <v-form>
+                    <v-text-field v-model="email" label="Email" name="email" prepend-icon="mdi-email" type="email"></v-text-field>
+                    <v-text-field v-model="password" label="Password" name="password" prepend-icon="mdi-lock" type="password"></v-text-field>
+                    <v-layout justify-center>
+                        <v-btn color="primary" @click="login">Login</v-btn>
+                    </v-layout>
+                </v-form>
+            </v-card-text>
+        </v-card>
+</template>
+
+<script>
+export default {
+    name: 'LoginForm',
+    props: {
+        users: Array,
+    },
+    data: () => ({
+        email: '',
+        password: '',
+    }),
+
+    methods: {
+        login() {
+            const user = this.users.find(user => user.email === this.email && user.password === this.password);
+
+            if (user) {
+                this.$emit('login', user);
+                this.$emit('show_message', true);
+                this.$emit('login_message', 'Inicio exitoso');
+                this.$emit('login_color', 'success');
+            } else {
+                this.$emit('show_message', true);
+                this.$emit('login_message', 'Vuelva a intentarlo');
+                this.$emit('login_color', 'error');
+            }
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
