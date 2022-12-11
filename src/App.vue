d8fcbbbd3cfc2bf5f1c09b554f18a0d0f8c96b84
Luis Urán <luisuran.bit@gmail.com>
2022-12-11 06:29:20 -0300

    Componente producto detalle

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,17 @@
       {{ snack_message }}
     </v-snackbar>
     <login-form :users="users" @login="logged_user = $event" @show_message="snackbar = $event" @login_message="snack_message = $event" @login_color="snack_color = $event"/>
-    <sign-up-form @new_user="addUser($event)" @show_message="snackbar = $event" @login_message="snack_message = $event" @login_color="snack_color = $event"/>
+    <sign-up-form @new_user="registerUser($event)" @show_message="snackbar = $event" @login_message="snack_message = $event" @login_color="snack_color = $event"/>
+    <producto-detalle :watch="watches[0]" @add_to_cart="addToCart($event)"/>
   </v-app>
 </template>
 
 <script>
 import LoginForm from './components/LoginForm.vue';
+import ProductoDetalle from './components/ProductoDetalle.vue';
 import SignUpForm from './components/SignUpForm.vue';
+import watches from './assets/datos/watches.json';
+import users from './assets/datos/users.json';
 
 export default {
   name: 'App',
@@ -18,31 +22,21 @@ export default {
   components: {
     LoginForm,
     SignUpForm,
+    ProductoDetalle,
   },
 
   data: () => ({
-    users: [
-      {
-        id: 1,
-        name: 'Luis Uran',
-        email: 'luisuran.bit@gmail.com',
-        password: '123456',
-      },
-      {
-        id: 2,
-        name: 'Pepe',
-        email: 'pepe@gmail.com',
-        password: '123',
-      }
-    ],
+    users: users,
+    watches: watches,
     logged_user : null,
     snackbar: false,
     snack_message: "",
     snack_color: "",
+    cart: [],
   }),
 
   methods: {
-    addUser(event) {
+    registerUser(event) {
       this.users.push({
         id: this.users.length + 1,
         name: event.name,
@@ -50,6 +44,20 @@ export default {
         password: event.password,
       });
       this.logged_user = this.users.find(user => user.email === event.email && user.password === event.password);
+    },
+    addToCart(event) {
+      const index = this.cart.findIndex(item => item.id === event.id);
+      if (index === -1) {
+        this.cart.push({
+          id: event.id,
+          name: event.name,
+          price: event.price,
+          picture: event.picture,
+          quantity: 1,
+        });
+      } else {
+        this.cart[index].quantity += 1;
+      }
     },
   },
 };
---
+++ src/components/ProductoDetalle.vue
@@ -0,0 +1,45 @@
+<template>
+ <v-card class="mx-auto my-auto" max-width="300">
+    <v-img :src="watch.picture"></v-img>
+    <v-layout justify-center>
+        <v-card-title class="text-center">{{tituloMayusculas}}</v-card-title>
+    </v-layout>
+    <v-card-text class="py-0">{{watch.description}}</v-card-text>
+    <v-layout justify-center>
+        <v-card-subtitle class="text-subtitle-2">${{watch.price}}</v-card-subtitle>
+    </v-layout>
+    <v-layout justify-center>
+        <v-btn color="primary" class="mb-3" @click="addToCart()">Agregar al carrito</v-btn>
+    </v-layout>
+ </v-card> 
+</template>
+
+<script>
+export default {
+    name: 'ProductoDetalle',
+
+    data: () => ({
+        cantidad: 0,
+    }),
+
+    props: {
+        watch: Object,
+    },
+
+    computed: {
+        tituloMayusculas() {
+            return this.watch.name.toUpperCase();
+        },
+    },
+
+    methods: {
+        addToCart() {
+            this.$emit('add_to_cart', this.watch);
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
