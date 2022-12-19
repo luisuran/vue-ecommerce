1d3952f4b198e7001d6302b0cde18bc8fed20562
Luis Urán <luisuran.bit@gmail.com>
2022-12-19 17:28:31 -0300

    Agrego pagina 404

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container>
+    <v-layout justify-center align-center>
+        <h1 class="grey--text text-h1">404</h1>
+    </v-layout>
+    <v-layout justify-center align-center>
+        <h2 class="grey--text text-h2">Página no encontrada</h2>
+    </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
